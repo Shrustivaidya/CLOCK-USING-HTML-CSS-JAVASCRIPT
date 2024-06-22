5145f5f2e6b1aa971b99b4ae2355ac1e83d45823
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-06-22 13:06:32 +0530

    Basic changes

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,8 +7,6 @@ setInterval(()=> {
     mrotation = 6*mtime;
     srotation = 6*stime;
 
-
-    
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
